b3a6da8c839b0a5d651e7785e05777546a2f475a
Danish Sharma <danishsharma13255@gmail.com>
2023-02-27 22:48:56 +0530

    Dashboard Component

--- src/layouts/dashboard/Dashboard.js
+++ src/layouts/dashboard/Dashboard.js
@@ -1,13 +1,18 @@
 import { useTheme } from "@emotion/react";
-import { Box, Stack } from "@mui/material";
-import React from "react";
+import { Avatar, Box, Divider, IconButton, Stack } from "@mui/material";
+import { Gear } from "phosphor-react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { faker } from "@faker-js/faker";
 import Logo from "../../assets/Images/logo.ico";
+import { Nav_Buttons } from "../../data/NavData";
 const DashboardLayout = () => {
   const theme = useTheme();
+  const [selected, setSelected] = useState(0);
   return (
     <>
       <Box
+        p={2}
         sx={{
           backgroundColor: theme.palette.background.paper,
           boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
@@ -15,18 +20,88 @@ const DashboardLayout = () => {
           width: 100,
         }}
       >
-        <Stack direction="column" sx={{ width: "100%" }}>
-          <Box
-            sx={{
-              backgroundColor: theme.palette.primary.main,
-              height: 64,
-              width: 64,
-              borderRadius: 1.5,
-            }}
-          >
-            {" "}
-            <img src={Logo} alt="logo" />
-          </Box>
+        <Stack
+          direction="column"
+          alignItems={"center"}
+          justifyContent="space-between"
+          sx={{ height: "100%" }}
+          spacing={3}
+        >
+          <Stack alignItems={"center"} spacing={4}>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: 1.5,
+              }}
+            >
+              {" "}
+              <img src={Logo} alt="logo" />
+            </Box>
+            <Stack
+              sx={{ width: "max-content" }}
+              direction="column"
+              alignItems="center"
+              spacing={3}
+            >
+              {Nav_Buttons.map((el) =>
+                el.index === selected ? (
+                  <Box
+                    p={1}
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: 1.5,
+                    }}
+                  >
+                    <IconButton
+                      sx={{ width: "max-content", color: "white" }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <IconButton
+                    onClick={() => {
+                      setSelected(el.index);
+                    }}
+                    sx={{ width: "max-content", color: "black" }}
+                    key={el.index}
+                  >
+                    {el.icon}
+                  </IconButton>
+                )
+              )}
+              <Divider sx={{ width: "48px" }} />
+              {selected === 3 ? (
+                <Box
+                  p={1}
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton sx={{ width: "max-content", color: "white" }}>
+                    <Gear />
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => {
+                    setSelected(3);
+                  }}
+                  sx={{ width: "max-content", color: "black" }}
+                >
+                  <Gear />
+                </IconButton>
+              )}
+            </Stack>
+          </Stack>
+
+          <Stack>
+            <Avatar src={faker.image.avatar()} />
+          </Stack>
         </Stack>
       </Box>
       <Outlet />
